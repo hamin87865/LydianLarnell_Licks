a96110f1df53c85ced00951c12f20fbed4a659e6
정하민 <hamin87865@gmail.com>
2026-04-21 11:55:57 +0900

    lydianlarnell_licks

--- server/lib/csp.ts
+++ server/lib/csp.ts
@@ -14,22 +14,33 @@ export function buildProductionHelmetConfig(): HelmetOptions {
         defaultSrc: [SELF],
         baseUri: [SELF],
         objectSrc: ["'none'"],
+
         scriptSrc: [
           SELF,
           UNSAFE_INLINE,
           "https://js.tosspayments.com",
+          "https://pay.tosspayments.com",
+          "https://payment-widget.tosspayments.com",
+          "https://payment-gateway.tosspayments.com",
+          "https://payment-gateway-sandbox.tosspayments.com",
           "https://www.youtube.com",
           "https://www.youtube.com/iframe_api",
           "https://s.ytimg.com",
         ],
+
         scriptSrcElem: [
           SELF,
           UNSAFE_INLINE,
           "https://js.tosspayments.com",
+          "https://pay.tosspayments.com",
+          "https://payment-widget.tosspayments.com",
+          "https://payment-gateway.tosspayments.com",
+          "https://payment-gateway-sandbox.tosspayments.com",
           "https://www.youtube.com",
           "https://www.youtube.com/iframe_api",
           "https://s.ytimg.com",
         ],
+
         connectSrc: [
           SELF,
           "https://api.tosspayments.com",
@@ -39,11 +50,13 @@ export function buildProductionHelmetConfig(): HelmetOptions {
           "https://payment-gateway.tosspayments.com",
           "https://payment-gateway-sandbox.tosspayments.com",
           "https://apigw.tosspayments.com",
+          "https://apigw-sandbox.tosspayments.com",
           "https://log.tosspayments.com",
           "https://event.tosspayments.com",
-          "https://apigw-sandbox.tosspayments.com",
+          "https://static.toss.im",
           "https://www.youtube.com",
         ],
+
         frameSrc: [
           SELF,
           "https://js.tosspayments.com",
@@ -55,6 +68,26 @@ export function buildProductionHelmetConfig(): HelmetOptions {
           "https://www.youtube.com",
           "https://www.youtube-nocookie.com",
         ],
+
+        childSrc: [
+          SELF,
+          "https://js.tosspayments.com",
+          "https://pay.tosspayments.com",
+          "https://tosspayments.com",
+          "https://payment-widget.tosspayments.com",
+          "https://payment-gateway.tosspayments.com",
+          "https://payment-gateway-sandbox.tosspayments.com",
+        ],
+
+        formAction: [
+          SELF,
+          "https://pay.tosspayments.com",
+          "https://tosspayments.com",
+          "https://payment-widget.tosspayments.com",
+          "https://payment-gateway.tosspayments.com",
+          "https://payment-gateway-sandbox.tosspayments.com",
+        ],
+
         imgSrc: [
           SELF,
           DATA,
@@ -64,15 +97,25 @@ export function buildProductionHelmetConfig(): HelmetOptions {
           "https://static.toss.im",
           "https://tosspayments.com",
         ],
+
         mediaSrc: [SELF, BLOB, "https://www.youtube.com"],
+
         styleSrc: [
           SELF,
           UNSAFE_INLINE,
           "https://fonts.googleapis.com",
           "https://js.tosspayments.com",
           "https://pay.tosspayments.com",
+          "https://static.toss.im",
         ],
-        fontSrc: [SELF, DATA, "https://fonts.gstatic.com", "https://js.tosspayments.com"],
+
+        fontSrc: [
+          SELF,
+          DATA,
+          "https://fonts.gstatic.com",
+          "https://js.tosspayments.com",
+          "https://static.toss.im",
+        ],
       },
     },
   };
